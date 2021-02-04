42b11692ca284f7e208b91f3474a2fb5df5bee62
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-02-04 19:37:16 +0100

    An test array added to the state and now is displaying its content on the screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {id: "as", name: 'Frankenstein'},
+        {id: "cb", name: 'Dracula'},
+        {id: "fg", name: 'Zombie'}, 
+      ]
+    }
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        {this.state.monsters.map( monster => <h2 key={monster.id}>{monster.name}</h2>)}
+      </div>
+    )
+  }
+} 
 
 export default App;
