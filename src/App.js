ed5e0036b13d4dd4632c0f3eb8e7523a49784997
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-02-06 18:57:50 +0100

    Input from App component was deleted and search component was created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 
 import {CardList} from './components/card-list/card-list-component';
+import {SearchBox} from './components/search-box/search-box-component';
 
 class App extends Component {
   constructor() {
@@ -27,7 +28,7 @@ class App extends Component {
 
     return (
       <div className="wrapper">
-        <input className="search-field" onChange={ e => this.setState({searchField: e.target.value})} type="text" placeholder={`find a bad guy`}/>
+        <SearchBox  placeholder={`Enter a guy name`} handleChange={ e => this.setState({ searchField: e.target.value }) }/>
         <CardList badGuys={filterBadGuys} />
       </div>
     )
