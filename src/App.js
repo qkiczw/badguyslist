93346ce9601cea21390ed49fc7b93c036bad40b7
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-04-15 12:39:16 +0200

    Badguys props was removed from badguy profile route, now badguy profile page display a chosen profile

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ class App extends Component {
           <Switch>
             {/* <Route path="/" exact component={HomePage} /> */}
             <Route path="/" exact render={(props)=> (<HomePage {...props} badGuys={this.state.badGuys} handleRelease={this.releaseBadGuy}/>)} />
-            <Route path="/badguy/:id" render={(props)=> (<BadguyProfile {...props}  />)} />
+            <Route path="/badguy/:id" render={(props)=> (<BadguyProfile {...props}  badGuys={this.state.badGuys} />)} />
           </Switch>
         </div>
       </BrowserRouter>
--- src/pages/badguy-profile-page/Badguy-profile.page.js
+++ src/pages/badguy-profile-page/Badguy-profile.page.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import './badguy-profile.styles.css'
 
 
 export const BadguyProfile = (props) => {
-    const { match } = props;
+    const badGuyId = props.match.params.id
+    
+    useEffect( ()=> {
+        fetch(`https://jsonplaceholder.typicode.com/users/${badGuyId}`)
+        .then( response => response.json()) 
+        .then( guy => setBadGuy({...guy}) ) 
+    },[])
+    
+    const [badGuy, setBadGuy] = useState({})
+
+
 
     return(
-        <div>
-        {console.log(match.params.id)}
+        <div className='bad-guy-wrapper'>
+        {console.log(badGuy)}
+
             <h2>Bad Guy Full Profile</h2>
-            <h3>This page will show you a badGuy number {match.params.id} full info</h3>
+            <h3>This page will show you a badGuy number {badGuyId} full info</h3>
+            <div>
+                <img alt={`Bad guy named ${badGuy.name}`} src={`https://robohash.org/${badGuyId}set=set2`}/>
+                <p>Ful name: {badGuy.name}</p>
+                <p>Username: {badGuy.username}</p>
+                <p>email: {badGuy.email}</p>
+                <p>phone: {badGuy.phone}</p>
+            </div>
         </div>
     )
 }
