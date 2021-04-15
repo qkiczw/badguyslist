6615cc0334b7001e8c91f49d5a7af55c14050b3a
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-04-15 11:55:20 +0200

    Title in Header is now changed and styled to a link to homepage

--- src/components/header/header-component.js
+++ src/components/header/header-component.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './header-style.css';
 
 
 export const Header = ( props ) => (
     <header>
-        <h1>{props.title}</h1>
+        <Link to={'/'}>
+            <h1>{props.title}</h1>
+        </Link>
     </header>
 );
